1107ec98b3cf3e76767d8ce7f702f695ac90595d
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-02 18:44:27 +0000

    Add day 2 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2019/Day2.java
+++ src/main/java/com/grahamedgecombe/advent2019/Day2.java
@@ -2,6 +2,7 @@ package com.grahamedgecombe.advent2019;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public final class Day2 {
@@ -15,5 +16,25 @@ public final class Day2 {
 		machine.poke(2, 2);
 		machine.evaluate();
 		System.out.println(machine.peek(0));
+
+		System.out.println(solve(program, 19690720));
+	}
+
+	public static int solve(List<Integer> program, int desiredOutput) {
+		for (int noun = 0; noun < 100; noun++) {
+			for (int verb = 0; verb < 100; verb++) {
+				var machine = new IntcodeMachine(program);
+				machine.poke(1, noun);
+				machine.poke(2, verb);
+				machine.evaluate();
+
+				int output = machine.peek(0);
+				if (output == desiredOutput) {
+					return noun * 100 + verb;
+				}
+			}
+		}
+
+		throw new IllegalArgumentException("No solution");
 	}
 }
