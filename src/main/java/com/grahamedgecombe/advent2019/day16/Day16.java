4b7340c07c7325f5b502994534d961f1a1f82175
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-16 23:08:31 +0000

    Add day 16 part 2 solution
    This commit also adds additional unit tests for part 1 (too lazy to
    split this out properly given how long this took!)

--- src/main/java/com/grahamedgecombe/advent2019/day16/Day16.java
+++ src/main/java/com/grahamedgecombe/advent2019/day16/Day16.java
@@ -4,19 +4,34 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.google.common.base.Preconditions;
 import com.grahamedgecombe.advent2019.Day;
 
-public final class Day16 extends Day<int[]> {
+public final class Day16 extends Day<String> {
 	private static final int[] PATTERN = { 0, 1, 0, -1 };
 
-	public static int[] transform(int[] list, int phases) {
-		for (int i = 0; i < phases; i++) {
-			list = transform(list);
-		}
-		return list;
+	private static int[] parse(String line) {
+		return line.chars()
+			.map(c -> c - '0')
+			.toArray();
 	}
 
-	private static int[] transform(int[] in) {
+	private static String getEightDigits(int[] list) {
+		return Arrays.stream(list)
+			.limit(8)
+			.mapToObj(Integer::toString)
+			.collect(Collectors.joining());
+	}
+
+	public static String transformPart1(String line, int phases) {
+		var list = parse(line);
+		for (int i = 0; i < phases; i++) {
+			list = transformPart1(list);
+		}
+		return getEightDigits(list);
+	}
+
+	private static int[] transformPart1(int[] in) {
 		var out = new int[in.length];
 
 		for (var i = 0; i < out.length; i++) {
@@ -33,23 +48,115 @@ public final class Day16 extends Day<int[]> {
 		return out;
 	}
 
+	private static int[] repeat(int[] in, int offset, int repeat) {
+		var out = new int[in.length * repeat - offset];
+		for (var i = 0; i < out.length; i++) {
+			out[i] = in[(i + offset) % in.length];
+		}
+		return out;
+	}
+
+	public static String transformPart2(String line, int phases, int repeat) {
+		var list = parse(line);
+		var offset = Integer.parseInt(line.substring(0, 7));
+
+		/* the optimization in transformPart2() only works on the RHS */
+		Preconditions.checkArgument(offset >= (list.length * repeat) / 2);
+
+		/*
+		 * The left-hand side of the pattern always forms a triangle of zeroes:
+		 *
+		 * digit 1: 1 ...
+		 * digit 2: 0 1 ...
+		 * digit 3: 0 0 0 1 ...
+		 * digit 4: 0 0 0 0 1 ...
+		 *
+		 * This means digits are only affected by their previous value of
+		 * themselves and any digits to the right. They are not affected by
+		 * digits to the left, so we can cut off digits from the left-hand side
+		 * and the output remains the same.
+		 *
+		 * Cutting off everything to the left of the offset reduces the input
+		 * size (in my case) by a factor of 10.
+		 *
+		 * This is implemented in repeat(), which handles repeating the input
+		 * and then subsequently cutting it in one pass (to reduce intermediate
+		 * memory usage).
+		 */
+		list = repeat(list, offset, repeat);
+
+		for (int i = 0; i < phases; i++) {
+			list = transformPart2(list);
+		}
+		return getEightDigits(list);
+	}
+
+	private static int[] transformPart2(int[] input) {
+		var output = new int[input.length];
+
+		/*
+		 * The part 1 algorithm is essentially matrix/vector multiplication,
+		 * where the matrix is dynamically generated based on the pattern.
+		 *
+		 * The 8x8 matrix (for example) looks something like this (? marks
+		 * numbers we don't really care about):
+		 *
+		 *  1 ? ? ? ? ? ? ?
+		 *  0 1 ? ? ? ? ? ?
+		 *  0 0 1 ? ? ? ? ?
+		 *  0 0 0 1 ? ? ? ?
+		 *  0 0 0 0 1 1 1 1
+		 *  0 0 0 0 0 1 1 1
+		 *  0 0 0 0 0 0 1 1
+		 *  0 0 0 0 0 0 0 1
+		 *
+		 * As well as the triangle of zeroes (noted earlier), note that the
+		 * bottom right quarter of the matrix is a triangle of ones.
+		 *
+		 * This makes the 8x8 transform look something like this for the RHS of
+		 * the output vector, assuming our input vector is
+		 * (a, b, c, d, e, f, g, h):
+		 *
+		 * e' = e + f + g + h = e + f'
+		 * f' =     f + g + h = f + g'
+		 * g' =         g + h = g + h'
+		 * h' =             h = h
+		 *
+		 * where + is defined as addition modulo 10.
+		 *
+		 * If we index our vector with numbers instead of letters, this is
+		 * just:
+		 *
+		 * out[i] = in[i] + out[i + 1]
+		 *
+		 * with a base case of 0 for out[len] (len=8 in this case).
+		 */
+		for (var i = output.length - 1; i >= 0; i--) {
+			var up = input[i];
+			var right = i != output.length - 1 ? output[i + 1] : 0;
+			output[i] = Math.abs(up + right) % 10;
+		}
+
+		return output;
+	}
+
 	public Day16() {
 		super(16);
 	}
 
 	@Override
-	public int[] parse(List<String> lines) {
-		return lines.get(0).chars()
-			.map(c -> c - '0')
-			.toArray();
+	public String parse(List<String> lines) {
+		return lines.get(0);
+
 	}
 
 	@Override
-	public Object solvePart1(int[] input) {
-		var output = transform(input, 100);
-		return Arrays.stream(output)
-			.limit(8)
-			.mapToObj(Integer::toString)
-			.collect(Collectors.joining());
+	public Object solvePart1(String input) {
+		return transformPart1(input, 100);
+	}
+
+	@Override
+	public Object solvePart2(String input) {
+		return transformPart2(input, 100, 10000);
 	}
 }
--- src/test/java/com/grahamedgecombe/advent2019/day16/Day16Test.java
+++ src/test/java/com/grahamedgecombe/advent2019/day16/Day16Test.java
@@ -5,14 +5,23 @@ import java.io.IOException;
 import com.grahamedgecombe.advent2019.Advent;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class Day16Test {
 	@Test
 	public void testPart1() throws IOException {
-		assertArrayEquals(new int[] { 0, 1, 0, 2, 9, 4, 9, 8 }, Day16.transform(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 4));
-
+		assertEquals("01029498", Day16.transformPart1("12345678", 4));
+		assertEquals("24176176", Day16.transformPart1("80871224585914546619083218645595", 100));
+		assertEquals("73745418", Day16.transformPart1("19617804207202209144916044189917", 100));
+		assertEquals("52432133", Day16.transformPart1("69317163492948606335995924319873", 100));
 		assertEquals("30550349", Advent.solvePart1(new Day16()));
+	}
+
+	@Test
+	public void testPart2() throws IOException {
+		assertEquals("84462026", Day16.transformPart2("03036732577212944063491565474664", 100, 10000));
+		assertEquals("78725270", Day16.transformPart2("02935109699940807407585447034323", 100, 10000));
+		assertEquals("53553731", Day16.transformPart2("03081770884921959731165446850517", 100, 10000));
+		assertEquals("62938399", Advent.solvePart2(new Day16()));
 	}
 }
