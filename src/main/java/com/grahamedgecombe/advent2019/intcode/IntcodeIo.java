5f8f10c2511d69ae4e8266681c534b5e716ff614
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-12-21 19:16:26 +0000

    Add interactive IntcodeIo implementation

--- src/main/java/com/grahamedgecombe/advent2019/intcode/IntcodeIo.java
+++ src/main/java/com/grahamedgecombe/advent2019/intcode/IntcodeIo.java
@@ -1,5 +1,8 @@
 package com.grahamedgecombe.advent2019.intcode;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
 public interface IntcodeIo {
 	IntcodeIo UNSUPPORTED = new IntcodeIo() {
 		@Override
@@ -10,6 +13,23 @@ public interface IntcodeIo {
 		@Override
 		public boolean write(long value) {
 			throw new UnsupportedOperationException();
+		}
+	};
+
+	IntcodeIo INTERACTIVE = new IntcodeIo() {
+		@Override
+		public long read() {
+			try {
+				return System.in.read();
+			} catch (IOException ex) {
+				throw new UncheckedIOException(ex);
+			}
+		}
+
+		@Override
+		public boolean write(long value) {
+			System.out.write((char) value);
+			return false;
 		}
 	};
 
